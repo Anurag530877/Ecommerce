5e4bf2f5d8421d44fb1e7beb8b89c170ecc3322d
Anurag Kumar Gupta <anuragkumargupa66@gmail.com>
2026-06-08 12:05:12 +0530

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+export default function HomePage(){
+    return(<>
+    <div className="container">
+        <div className="row justify-content-center">
+            <div className="col-md-4 shadow text-center">
+                <h3>Anurag Tech</h3>
+            </div>
+            <div className="col-md-5">
+                </div> 
+             </div>
+    </div>
+    </>)
+}
